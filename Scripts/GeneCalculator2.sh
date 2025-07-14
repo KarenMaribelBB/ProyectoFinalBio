2022c8437c74a45fc91f4a98e60e2460bd15ca53
Karen Maribel <karencitamaribel4@gmail.com>
2025-07-14 10:31:50 -0500

    Se añadió segundo GeneCalculator para hacer filogenias individuales por genes

---
+++ Scripts/GeneCalculator2.sh
@@ -0,0 +1,19 @@
+# Para alinear las seciencias
+cd Data
+for x in *.fasta; do ../Scripts/muscle3.8.31_i86linux64 -in $x -out musscle_$x; done
+
+# Cargar iqtree
+module load iqtree/2.2.2.6
+
+# Para hacer filogenias
+for muscle in musscle_*
+do
+  iqtree2 -s ${muscle} -m MFP -bb 1000 -alrt 1000 -nt AUTO
+done
+
+# Ordenar los archivos en carpetas independientes
+cd ..
+mv Data/*.treefile Iqtree/
+mv Data/musscle_* Muscle/
+
+
