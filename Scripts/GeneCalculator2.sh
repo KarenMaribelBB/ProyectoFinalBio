72669e260de7ee7108ef455e246a41b2fef3666c
Karen Maribel <karencitamaribel4@gmail.com>
2025-07-14 10:37:14 -0500

    Modificación de GeneCalculator2.sh

--- Scripts/GeneCalculator2.sh
+++ Scripts/GeneCalculator2.sh
@@ -5,7 +5,7 @@ for x in *.fasta; do ../Scripts/muscle3.8.31_i86linux64 -in $x -out musscle_$x; 
 # Cargar iqtree
 module load iqtree/2.2.2.6
 
-# Para hacer filogenias
+# Para hacer filogenias con bootstrap
 for muscle in musscle_*
 do
   iqtree2 -s ${muscle} -m MFP -bb 1000 -alrt 1000 -nt AUTO
